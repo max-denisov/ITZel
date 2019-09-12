81d4d0f96696fc37b99dd72bd230590531e00f9f
Scealang <54307873+Scealang@users.noreply.github.com>
2019-09-12 12:25:09 +0300

    Add files via upload

---
+++ src/main/Utils/src/Utils.java
@@ -0,0 +1,11 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Utils {
+    private static final Logger log = LogManager.getLogger(Model.class);
+
+    public static void main(String[] args){
+        log.log(Level.INFO, "Hello World!");
+    }
+}
